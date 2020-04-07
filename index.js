ff7a19d183667a23c9d5e115f147afaa2bfc881a
Thiago Kienbaum <thiago.kienbaum@hotmail.com>
2020-04-07 07:57:44 -0300

    Incluido rotas CRUD.

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const projects = [
     {
         "id": "1",
         "title": "Projeto01",
-        "tasks": ["tarefa01","tarefa04","tarefa03"]
+        "tasks": ["tarefa01","tarefa02","tarefa03"]
     },
     {
         "id": "2",
         "title": "Projeto02",
-        "tasks": ["tarefa01","tarefa04","tarefa03"]
+        "tasks": ["tarefa04","tarefa05","tarefa06"]
     },
     {
         "id": "3",
         "title": "Projeto03",
-        "tasks": ["tarefa01","tarefa04","tarefa03"]
+        "tasks": ["tarefa07","tarefa08","tarefa09"]
     }
 ];
 
 function logRequests(req, res, next) {
     console.time('Request');
     console.count('Requests')
-    console.log(`Method: ${req.method}, URL: ${req.url}.`)
+    console.log(`Method: ${req.method}, URL: ${req.url}.`);
     next();
     console.timeEnd('Request');
 };
@@ -41,17 +41,17 @@ function validateProjectID(req, res, next) {
 server.use(express.json());
 server.use(logRequests);
 
-//CRUD
-    //Cadastrar novo projeto
 server.post('/projects', (req, res) => {
-    const { id } = req.body;
+    const { id } = req.body
     const { title } = req.body;
-    projects.push({ "id": id, "title": title })
+    projects.push({ "id": id, "title": title });
+    return res.json(projects);
 });
 
-    //Cadastrar nova task por ID do projeto
+//Criar uma nova task
 server.post('/projects/:id/tasks', validateProjectID, (req, res) => {
-    
+    req.projectFound.tasks.push(req.body.tasks);
+    return res.json(req.projectFound);    
 });
 
 server.get('/projects', (req, res) => {
@@ -62,14 +62,17 @@ server.get('/projects/:id', validateProjectID, (req, res) => {
     return res.json(req.projectFound)
 });
 
-    //Alterar o nome do projeto por ID
 server.put('/projects/:id', validateProjectID, (req, res) => {
-
+    req.projectFound.title = req.body.title;
+    return res.json(req.projectFound);
 });
 
     //Deletar projeto por ID
 server.delete('/projects/:id', validateProjectID, (req, res) => {
+    var projectToDelete = projects.indexOf(req.projectFound)
 
+
+    return res.json(projects);
 });
 
 server.listen(3333);
